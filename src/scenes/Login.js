8817343281e8337acc73fb06cdff3354291ea3d6
“Keren <kerencharles4@gmail.com>
2022-02-16 10:34:01 -0500

    set up signup page and routing

---
+++ src/scenes/Login.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Login() {
+    return (
+        <>
+            <h1>Login</h1>
+            <p>Not a user?<Link to="/signup">SignUp</Link></p>
+        </>
+    )
+
+}
